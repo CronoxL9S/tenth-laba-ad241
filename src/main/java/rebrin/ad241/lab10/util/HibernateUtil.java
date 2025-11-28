768296720760dca331df4d17b3db95cf4ca9fe4c
CronoxL9S <rebrin2018@gmail.com>
2025-11-29 00:22:12 +0200

    Додаю вихідний код для util

--- src/main/java/rebrin/ad241/lab10/util/HibernateUtil.java
+++ src/main/java/rebrin/ad241/lab10/util/HibernateUtil.java
@@ -1,1 +1,25 @@
+package rebrin.ad241.lab10.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Помилка створення SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
